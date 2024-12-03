843c50afc5e67cd2b74700cc13784115ed2e812f
trigger67 <10185784+trigger67@users.noreply.github.com>
2024-12-03 11:43:02 +0100

    chore: update script to rename timestamping tutorial

--- packages/database/drizzle/migrations/0044_pink_thing.sql
+++ packages/database/drizzle/migrations/0044_pink_thing.sql
@@ -1,2 +1,3 @@
+UPDATE content.tutorials SET NAME = 'pbn-certificate-timestamping' where name = 'bcert-verification';
 ALTER TABLE "content"."courses" RENAME COLUMN "paid_start_date" TO "start_date";--> statement-breakpoint
 ALTER TABLE "content"."courses" RENAME COLUMN "paid_end_date" TO "end_date";
