73dad3ddfd6a5ffa7a16ace72e8550a10ca1fda7
Doe <89263198+Flashmaxi@users.noreply.github.com>
2024-12-08 22:35:47 +0100

    feat(header): redesign

--- apps/web/src/components/Header/meta-elements.tsx
+++ apps/web/src/components/Header/meta-elements.tsx
@@ -1,14 +1,17 @@
-import { Link } from '@tanstack/react-router';
+import { Link, useLocation } from '@tanstack/react-router';
 import { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { IoLogOutOutline } from 'react-icons/io5';
+
+import { Button } from '@blms/ui';
 
 import { useGreater } from '#src/hooks/use-greater.js';
 import { useSmaller } from '#src/hooks/use-smaller.js';
 import { AppContext } from '#src/providers/context.js';
 import { getPictureUrl } from '#src/services/user.js';
+import { logout } from '#src/utils/session-utils.ts';
 
-import SignInIconDark from '../../assets/icons/profile_log_in_dark.svg';
-import SignInIconLight from '../../assets/icons/profile_log_in_light.svg';
+import SignInIconLight from '../../assets/icons/sing-in.svg';
 
 import { LanguageSelector } from './language-selector.tsx';
 
@@ -24,11 +27,16 @@ export const MetaElements = ({
 }: MetaElementsProps) => {
   const { t } = useTranslation();
   const { user, session } = useContext(AppContext);
+  const location = useLocation();
   const isLoggedIn = !!session;
   const isMobile = useSmaller('lg');
   const isScreenLg = useGreater('lg');
 
   const pictureUrl = getPictureUrl(user);
+
+  const isOnDashboard = location.pathname
+    .replace(/^\/[a-z-]+\/?/, '')
+    .startsWith('dashboard');
 
   return (
     <div className="flex flex-row place-items-center gap-6 md:gap-2 lg:gap-6 ml-auto max-lg:mx-auto">
@@ -37,42 +45,46 @@ export const MetaElements = ({
         variant={variant}
       />
 
-      {
-        <>
-          {isLoggedIn && !isMobile && (
-            <Link className="flex" to="/dashboard/courses">
-              <button className="cursor-pointer text-white">
-                <img
-                  src={
-                    pictureUrl
-                      ? pictureUrl
-                      : variant === 'light'
-                        ? SignInIconLight
-                        : SignInIconDark
-                  }
-                  alt={t('auth.signIn')}
-                  className="size-12 rounded-full"
-                />
-              </button>
-            </Link>
+      {isLoggedIn && !isMobile && (
+        <Link className="flex" to="/dashboard/courses">
+          {isOnDashboard ? (
+            <button
+              className={`cursor-pointer rounded-[16px] py-[14px] px-[18px] ${
+                variant === 'light'
+                  ? 'bg-darkOrange-2 text-black hover:bg-darkOrange-1 active:bg-darkOrange-1 active:text-darkOrange-5'
+                  : 'bg-newBlack-3 text-white hover:bg-darkHover'
+              }`}
+              onClick={async () => {
+                await logout();
+                window.location.reload();
+              }}
+            >
+              <IoLogOutOutline size={24} />
+            </button>
+          ) : (
+            <button className="cursor-pointer text-white">
+              <img
+                src={pictureUrl ? pictureUrl : SignInIconLight}
+                alt={t('auth.signIn')}
+                className={`rounded-full ${pictureUrl ? 'size-12' : 'size-14'}`}
+              />
+            </button>
           )}
+        </Link>
+      )}
 
-          {!isLoggedIn && (
-            <div className="flex flex-row gap-2 lg:gap-4">
-              <button
-                className="cursor-pointer text-white"
-                onClick={onClickLogin}
-              >
-                <img
-                  src={variant === 'light' ? SignInIconLight : SignInIconDark}
-                  alt={t('auth.signIn')}
-                  className="size-12"
-                />
-              </button>
-            </div>
-          )}
-        </>
-      }
+      {!isLoggedIn && (
+        <div className="flex flex-row gap-2 lg:gap-4">
+          <Button
+            size={'l'}
+            variant={'loginButton'}
+            className="cursor-pointer text-white"
+            onClick={onClickLogin}
+          >
+            {t('menu.login')}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
