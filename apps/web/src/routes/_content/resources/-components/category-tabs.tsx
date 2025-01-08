15ee9a917603e6c440518a47824afa4a32c0f219
Doe <89263198+Flashmaxi@users.noreply.github.com>
2025-01-08 11:37:07 +0100

    fix(resource menu): minor adjustments

--- apps/web/src/routes/_content/resources/-components/category-tabs.tsx
+++ apps/web/src/routes/_content/resources/-components/category-tabs.tsx
@@ -14,43 +14,48 @@ export const CategoryTabs = ({ resourceActiveCategory }: CategoryTabsProps) => {
   const { t } = useTranslation();
 
   return (
-    <nav className="md:flex flex-wrap w-full justify-center items-center md:gap-6 lg:gap-8 xl:gap-10 md:px-5 mb-10 hidden">
-      {RESOURCES_CATEGORIES.map((resourceCategory) => (
-        <Link
-          key={resourceCategory.name}
-          to={`/resources/${resourceCategory.name}`}
-          onClick={(event: React.MouseEvent<HTMLAnchorElement>) =>
-            resourceCategory.unreleased && event.preventDefault()
-          }
-          className={cn(
-            'group',
-            resourceCategory.unreleased
-              ? 'cursor-not-allowed opacity-50'
-              : resourceCategory.name === resourceActiveCategory
-                ? 'scale-125 filter-newOrange1'
-                : '',
-          )}
-        >
-          <div className="flex justify-center items-center p-2 lg:p-4 border border-white rounded-xl">
-            <img
-              src={resourceCategory.image}
-              alt={resourceCategory.name}
-              className="filter-white max-w-7 lg:max-w-9"
-            />
+    <nav className="md:flex flex-wrap w-full justify-center items-center md:gap-6 lg:gap-8 xl:gap-10 md:px-5 mb-14 hidden">
+      {RESOURCES_CATEGORIES.map((resourceCategory) => {
+        const categoryTitle = capitalizeFirstWord(
+          t(`resources.${resourceCategory.name}.title`),
+        );
+        const isSingleWord = categoryTitle.split(' ').length === 1;
+
+        return (
+          <Link
+            key={resourceCategory.name}
+            to={`/resources/${resourceCategory.name}`}
+            onClick={(event: React.MouseEvent<HTMLAnchorElement>) =>
+              resourceCategory.unreleased && event.preventDefault()
+            }
+            className={cn(
+              'group relative',
+              resourceCategory.unreleased
+                ? 'cursor-not-allowed opacity-50'
+                : resourceCategory.name === resourceActiveCategory
+                  ? 'scale-125 filter-newOrange1'
+                  : '',
+            )}
+          >
+            <div className="flex justify-center items-center p-2 lg:p-2.5 border border-white rounded-xl">
+              <img
+                src={resourceCategory.image}
+                alt={resourceCategory.name}
+                className="filter-white max-w-[25px]"
+              />
+            </div>
             <span
               className={cn(
-                'text-white opacity-0 max-lg:text-sm font-medium leading-[1.43] tracking-[0.17px] max-w-0 inline-flex whitespace-nowrap transition-[max-width_opacity] overflow-hidden ease-in-out duration-700',
-                resourceCategory.name !== resourceActiveCategory &&
-                  'group-hover:max-w-96 group-hover:opacity-100 group-hover:ml-3 lg:group-hover:ml-4 group-focus:max-w-96 group-focus:opacity-100 group-focus:ml-3 lg:group-focus:ml-4',
+                'absolute md:-left-6 xl:-left-7 right-0 text-center body-14px mx-auto text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 w-[100px]',
+                resourceCategory.unreleased ? 'pointer-events-none' : '',
+                isSingleWord ? '-bottom-6' : '-bottom-11',
               )}
             >
-              {capitalizeFirstWord(
-                t(`resources.${resourceCategory.name}.title`),
-              )}
+              {categoryTitle}
             </span>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        );
+      })}
     </nav>
   );
 };
