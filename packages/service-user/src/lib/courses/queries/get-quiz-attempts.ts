08a68cab2b06922bda8340c4463ee22e21675bde
trigger67 <10185784+trigger67@users.noreply.github.com>
2025-01-24 14:45:50 +0100

    refactor: remove unused components

--- packages/service-user/src/lib/courses/queries/get-quiz-attempts.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from '@blms/database';
-import type { CourseQuizAttempts } from '@blms/types';
-
-export const getQuizAttemptsQuery = (uid: string) => {
-  return sql<CourseQuizAttempts[]>`
-    SELECT * FROM users.quiz_attempts
-    WHERE uid = ${uid};
-  `;
-};
