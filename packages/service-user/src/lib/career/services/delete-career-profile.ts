3a43bec2c7e94ffded2ef82fa6bd50d655cbe34a
Kevin Mulier <kevin.mulier@outlook.fr>
2025-01-28 10:35:21 +0100

    feat: remove cv from s3 on career profile deletion

--- packages/service-user/src/lib/career/services/delete-career-profile.ts
+++ packages/service-user/src/lib/career/services/delete-career-profile.ts
@@ -1,14 +1,29 @@
 import type { JoinedCareerProfile } from '@blms/types';
 
+import { firstRow } from '@blms/database';
 import type { Dependencies } from '../../../dependencies.js';
 import { deleteCareerProfileQuery } from '../queries/delete-career-profile.js';
+import { getCareerProfileIdQuery } from '../queries/get-career-profile.js';
 
 interface Options {
   uid: string;
 }
 
-export const createDeleteCareerProfile = ({ postgres }: Dependencies) => {
-  return ({ uid }: Options): Promise<JoinedCareerProfile[]> => {
+export const createDeleteCareerProfile = ({ postgres, s3 }: Dependencies) => {
+  return async ({ uid }: Options): Promise<JoinedCareerProfile[]> => {
+    const careerProfileId = await postgres
+      .exec(getCareerProfileIdQuery(uid))
+      .then(firstRow)
+      .then((a) => a?.id ?? null);
+
+    if (careerProfileId) {
+      try {
+        await s3.delete(`cvs/${careerProfileId}`);
+      } catch (error) {
+        console.error('Failed to delete related CV', careerProfileId, error);
+      }
+    }
+
     return postgres.exec(deleteCareerProfileQuery(uid));
   };
 };
